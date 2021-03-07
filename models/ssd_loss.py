7c26699b3227b3477abed90ab4c1581b2f5f304b
Igor <i.v.olkhovsky@gmail.com>
2021-03-07 16:50:51 +0300

    fix bug in ssd/retinanet loss

--- models/ssd_loss.py
+++ models/ssd_loss.py
@@ -2,21 +2,23 @@ import torch
 import torch.nn as nn
 
 from utils.bounding_box import compute_iou
-from models.focal_loss import FocalLoss
 from models.smooth_l1_loss import SmoothL1Loss
 
 
-class RetinaNetLoss(nn.Module):
+class SSDLoss(nn.Module):
 
-    def __init__(self, box_codec, anchors_cnt=6, classes_cnt=21, classification_weight=1., regression_weight=1.):
-        super(RetinaNetLoss, self).__init__()
+    def __init__(self, box_codec, anchors_cnt=6, classes_cnt=21, classification_weight=1., regression_weight=1.,
+                 hard_negatives_ratio=3.):
+        super(SSDLoss, self).__init__()
         self.anchors_cnt = anchors_cnt
         self.classes_cnt = classes_cnt
         self.box_codec = box_codec
         self.classifcation_w = classification_weight
         self.regression_w = regression_weight
         self.regression_criterion = SmoothL1Loss()
-        self.classification_criterion = FocalLoss()
+        self.classification_criterion = nn.CrossEntropyLoss(reduction="sum")
+        assert hard_negatives_ratio > 0
+        self.hard_negatives_ratio = hard_negatives_ratio
 
     def forward(self, classification_preds, boxes_preds, anchors, target_boxes, target_labels, iou_thresh=0.5):
         assert isinstance(classification_preds, (list, torch.Tensor))
@@ -41,14 +43,21 @@ class RetinaNetLoss(nn.Module):
         positive_anchors_cnt = torch.sum(positive_anchors_mask.int())
         normalization_cnt = torch.clamp(positive_anchors_cnt, min=1)
 
-        classification_targets = torch.zeros(size=classification_preds.size())
-        classification_targets[:, 0] = 1.
-        target_confidences = nn.functional.one_hot(target_labels[target_ids_for_anchors][positive_anchors_mask],
-                                                   num_classes=self.classes_cnt).float()
-        classification_targets[positive_anchors_mask] = target_confidences
-        classification_loss = self.classification_criterion(pred_logits=classification_preds,
-                                                            target=classification_targets)
-        classification_loss = torch.sum(classification_loss) / normalization_cnt
+        positive_anchors_targets = target_labels[target_ids_for_anchors[positive_anchors_mask]]
+        positive_classification_loss = self.classification_criterion(
+            input=classification_preds[positive_anchors_mask],
+            target=positive_anchors_targets)
+        negative_anchors_mask = torch.logical_not(positive_anchors_mask)
+        background_preds = classification_preds[negative_anchors_mask]
+        background_max_confs, _ = torch.max(background_preds[:, 1:], dim=1)
+        _, hard_negatives = zip(*sorted(zip(background_max_confs, background_preds), reverse=True, key=lambda x: x[0]))
+        hard_negatives = torch.stack(hard_negatives)
+        hard_negatives_cnt = int(self.hard_negatives_ratio * normalization_cnt)
+        negative_classification_loss = self.classification_criterion(
+            input=hard_negatives[:hard_negatives_cnt],
+            target=torch.as_tensor([0] * hard_negatives_cnt))
+
+        classification_loss = (positive_classification_loss + negative_classification_loss) / normalization_cnt
 
         regression_loss = 0.
         if positive_anchors_cnt:
