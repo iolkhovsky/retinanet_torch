01bddd948fde4b6dbf04761ab01198961e4b2262
Igor <i.v.olkhovsky@gmail.com>
2021-03-08 14:39:37 +0300

    loss minor update

--- models/retinanet_loss.py
+++ models/retinanet_loss.py
@@ -27,7 +27,7 @@ class RetinaNetLoss(nn.Module):
         if type(boxes_preds) == list:
             boxes_preds = torch.stack(boxes_preds)
         if type(anchors) == list:
-            anchors = torch.stack(anchors).to(classification_preds.device)
+            anchors = torch.stack(anchors).to(target_boxes.device)
         assert len(classification_preds) == len(boxes_preds) == len(anchors)
         assert len(target_boxes) == len(target_labels)
         assert iou_thresh > 0.1
